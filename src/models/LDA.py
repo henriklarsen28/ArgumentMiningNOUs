1b43f4f715fc7995ee99c539ec41a62fbd80f466
Brilleslangen <nicolaiSivesind@gmail.com>
2024-04-14 13:21:24 +0200

    Added prediciton method for topics

--- src/models/LDA.py
+++ src/models/LDA.py
@@ -6,10 +6,12 @@ from gensim.corpora import Dictionary
 
 
 class LDA:
-    def __init__(self, data: list[str], num_topics=5, language='nb_core_news_md'):
+    def __init__(self, data: list[str], num_topics=5, no_below=15, no_above=0.5, passes=10, language='nb_core_news_md'):
         self.data = data
+        self.preprocessed_data = self._preprocess()
         self.nlp = spacy.load(language)
-        self.model = self._build_LDA_model(num_topics)
+        self.corpus, self.dictionary = self._build_corpus(no_below=no_below, no_above=no_above)
+        self.model = self._build_LDA_model(num_topics, passes)
 
     def _preprocess(self) -> list[list[str]]:
         """
@@ -30,7 +32,8 @@ class LDA:
 
             cleaned_noun_chunks = []
             for chunk in doc.noun_chunks:
-                clean_chunk = ' '.join([token.lemma_.lower() for token in chunk if not token.is_stop and token.is_alpha])
+                clean_chunk = ' '.join(
+                    [token.lemma_.lower() for token in chunk if not token.is_stop and token.is_alpha])
                 if clean_chunk:
                     cleaned_noun_chunks.append(clean_chunk)
 
@@ -40,15 +43,32 @@ class LDA:
 
         return preprocessed
 
-    def _build_corpus(self) -> tuple[list[list[tuple[int, int]]], Dictionary]:
-        preprocessed_docs = self._preprocess()
-        dictionary = corpora.Dictionary(preprocessed_docs)
-
-        dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
-        corpus = [dictionary.doc2bow(doc) for doc in preprocessed_docs]
+    def _build_corpus(self, no_below=15, no_above=0.5) -> tuple[list[list[tuple[int, int]]], Dictionary]:
+        dictionary = corpora.Dictionary(self.preprocessed_data)
+        dictionary.filter_extremes(no_below=no_below, no_above=no_above, keep_n=100000)
+        corpus = [dictionary.doc2bow(doc) for doc in self.preprocessed_data]
         return corpus, dictionary
 
-    def _build_LDA_model(self, num_topics):
-        corpus, dictionary = self._build_corpus()
-        lda_model = models.LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary, passes=10)
+    def _build_LDA_model(self, num_topics, passes):
+        lda_model = models.LdaModel(corpus=self.corpus, num_topics=num_topics, id2word=self.dictionary, passes=passes)
         return lda_model
+
+    def predict_topics(self, relevancy=False):
+        predictions = []
+        for i, bow in enumerate(self.corpus):
+            topic_distribution = self.model.get_document_topics(bow)
+
+            sorted_topics = sorted(topic_distribution, key=lambda x: x[1], reverse=True)
+            top_topic, top_prob = sorted_topics[0]  # Top topic and its probability
+
+            prediction = {'topic': top_topic, 'confidence': round(top_prob, 2)}
+            predictions.append(prediction)
+
+            if relevancy:
+                top_topic_words = self.model.show_topic(top_topic, topn=1000)
+                top_words = [word for word, prob in top_topic_words]
+                preprocessed_words_set = set(self.preprocessed_data[i])
+                relevant_words = [word for word in top_words if word in preprocessed_words_set]
+                prediction['relevant_words'] = relevant_words
+
+        return predictions
