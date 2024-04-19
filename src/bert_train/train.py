72bb363f283f5afbc5e40d61636931c97576b3db
Brilleslangen <nicolaiSivesind@gmail.com>
2024-04-19 22:06:02 +0200

    Refactor

--- src/bert_train/train.py
+++ src/bert_train/train.py
@@ -1,6 +1,15 @@
-from icl_argument_classification import icl_train
-from lda_argument_classification import lda_train
+from finetuning import train_and_save_classifiers
 
-icl_train()
+
+def lda_train():
+    path = "../../dataset/LDA_Arguments.csv"
+    train_and_save_classifiers('LDA-Classifier', num_epochs=6, csv_path=path)
+
+
+def icl_train():
+    path = '../../dataset/cleaned_arguments_in_context_learning.csv'
+    train_and_save_classifiers('ICL-Classifier', num_epochs=6, csv_path=path)
+
+
 lda_train()
-
+icl_train()
