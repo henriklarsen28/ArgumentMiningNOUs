c40520d72665a8a00a378cf21454245990ff2671
Brilleslangen <nicolaiSivesind@gmail.com>
2024-04-20 11:02:57 +0200

    Save before refactor for regression

--- src/bert_train/sentiment-finetuning.py
+++ src/bert_train/sentiment-finetuning.py
@@ -6,7 +6,7 @@ path = "../../dataset/norec.csv"
 
 
 def train():
-    train_and_save_classifiers('Sentiment-Classifier', num_epochs=2, csv_path=path)
+    train_and_save_classifiers('Sentiment-Classifier', num_epochs=3, csv_path=path)
     # load_and_evaluate('../../classifiers/Raw-Classifier', csv_path=path)
 
 
@@ -14,10 +14,9 @@ def extract_dataset():
     dataset = load_dataset('ltg/norec', split='train')
     df = dataset.to_pandas()
 
-    df['rating'] = df['rating'] - 1
-
+    df['rating'] = df['rating'] - 1  # reduce to index
     df = df.rename(columns={'rating': 'label'})
-    df = df[['label', 'text']][: len(df)//2]
+    df = df[['label', 'text']][: len(df) // 2]
     labels = set(df['label'])
 
     print(labels)
@@ -27,6 +26,7 @@ def extract_dataset():
 def load():
     df = pd.read_csv(path)
     print(df.head())
+
 
 # extract_dataset()
 train()
--- src/bert_train/train.py
+++ src/bert_train/train.py
@@ -2,14 +2,15 @@ from finetuning import train_and_save_classifiers
 
 
 def lda_train():
+    print('Train LDA')
     path = "../../dataset/LDA_Arguments.csv"
     train_and_save_classifiers('LDA-Classifier', num_epochs=6, csv_path=path)
 
 
 def icl_train():
+    print('Train ICL')
     path = '../../dataset/cleaned_arguments_in_context_learning.csv'
     train_and_save_classifiers('ICL-Classifier', num_epochs=6, csv_path=path)
 
 
-lda_train()
 icl_train()
